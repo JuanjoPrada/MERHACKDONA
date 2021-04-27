5d0269b014fa9454df701a12de0f6734574716e4
Natalia Canales <nataliacanales95@outlook.com>
2021-04-27 13:04:50 +0200

    Admin can edit stores and products now, and delete them

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -6,19 +6,6 @@ const Store = require('./../models/store.model')
 // http://localhost:3000/api/stores
 router.get('/', (req, res) => {
 
-    const location = {
-        type: "Point",
-        coordinates: {
-            latitude: 40.369240,
-            longitude: -3.688777
-        }
-    }
-
-    Store
-        .create({ name: "Merhackona", location })
-        .then(() => res.redirect('/'))
-        .catch(err => console.log(err))
-
     Store
         .find()
         .then(stores => res.json(stores))
